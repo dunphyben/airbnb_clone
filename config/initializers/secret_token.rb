ef33f868a6e00c02cdb0d1a960258c870f359eff
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-04-16 18:57:18 -0700

    Initialize empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airbnb::Application.config.secret_key_base = '9f7f8e50002f93a8a7f082e8158c0c93ef51f664ec588a1e8f3cb321d7a963e307f69daa3a60095ae0bc217a376609a3640dc6f35c9d2958f5856a9673db519d'
