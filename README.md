28a737f873fe175bb9edeebe9c01bd6f8399136f
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-04-17 16:09:43 -0700

    Add Capybara tests for User visits index, sign up, sign in. Add readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 * As a user, I want to be able to sign up and log in/out of the site
 * As a user, I want to be able to view listings of BnBs for rent
+
+###### Epicodus Week 9, Day 4
--- spec/features/visitor_visits_index_page_spec.rb
+++ spec/features/visitor_visits_index_page_spec.rb
@@ -1,8 +1,40 @@
 require 'spec_helper'
 
+def visiting
+  visit root_path
+  page.should have_content 'Sign up'
+  page.should have_content 'Login'
+  page.should have_content 'User Index'
+end
+
 describe User do
   it 'allows visitors to see the home page' do
-    visit root_path
-    page.should have_content 'User Index'
+    visiting
+    click_link 'Sign up'
+
+  end
+  it 'allows visitors to sign up as users' do
+    visiting
+    click_link 'Sign up'
+    page.should have_content 'Sign up'
+    email = Faker::Internet.email
+    fill_in 'Email', with: email
+    password = Faker::Internet.password
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+  it 'allows existing users to sign in' do
+    visiting
+    user = FactoryGirl.create(:user)
+    click_link 'Login'
+    page.should have_content 'Sign in'
+    email = user.email
+    fill_in 'Email', with: email
+    password = user.password
+    fill_in 'Password', with: password
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 end
